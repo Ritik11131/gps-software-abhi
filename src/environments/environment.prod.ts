b261c868ef82aa864ec64939e3daeabacbfc65c9
Ritik Gupta <ritik11131@gmail.com>
2026-01-14 17:14:12 +0530

    Changes the api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   hostURL: "https://gpsvts.in",  
-  baseAPIUrl: "https://api.baliniot.in/api/",  // Fixed typo
+  baseAPIUrl: "http://api.gpsvts.in/api/",  // Fixed typo
   onDemand: {
     baseUrl: "https://gpsvts.in",
   },
