a346f69ad34732f09a3b9d3b33302ac59dfc4763
Koder <codergautam1@gmail.com>
2026-04-21 16:15:06 +0530

    removed session expired

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,5 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
-import { PushNotificationService } from './features/http-services/push-notification.service';
-import { ActivityService } from './features/shared/user/services/activity.service';
-import { SessionService } from './features/http-services/session.service';
-import { Router } from '@angular/router';
-import { BsModalService } from 'ngx-bootstrap/modal';
 import { environment } from 'src/environments/environment.prod';
 
 
@@ -14,22 +9,9 @@ import { environment } from 'src/environments/environment.prod';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  // constructor(private swUpdate: SwUpdate, private pushService: PushNotificationService) {}
-  @ViewChild("beforeExpired")
-  beforeExpired!: TemplateRef<any>;
-
-  @ViewChild("expired")
-  expired!: TemplateRef<any>;
-  seconds: number | undefined;
   ngOnInit() {
-    
-   
   }
   constructor(
-    private activityService: ActivityService,
-    private sessionService: SessionService,
-    private router: Router,
-    private bsModerService : BsModalService,
     private swUpdate: SwUpdate
   ) {
     if (environment.production && this.swUpdate.isEnabled) {
@@ -37,10 +19,6 @@ export class AppComponent implements OnInit {
         this.promptUser();
       });
     }
-    
-    activityService.secondsLeft.subscribe(
-      (seconds) => (this.seconds = seconds)
-    );
   }
 
   promptUser() {
@@ -50,20 +28,5 @@ export class AppComponent implements OnInit {
         alert('The app will update when you Refersh the application.');
       });
     }
-  }
-  
-  ngAfterViewInit(): void {
-    this.activityService.passTemplate(this.beforeExpired, this.expired);
-  }
-  continueSession() {
-    this.activityService.continueSession();
-  }
-  endSession() {
-    this.activityService.endSession();
-  }
-  signIn() {
-    this.bsModerService.hide()
-    this.activityService.releaseSessionLock();
-    this.sessionService.logout();
   }
 }
